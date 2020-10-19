8ece83226fe1a4d981c84fa5fd7e1a62cf923541
root <charlielbj@163.com>
2020-10-19 09:03:06 +0800

    rollback version

--- src/container/could/onsoft.js
+++ src/container/could/onsoft.js
@@ -17,49 +17,63 @@ class Onsoft extends Component {
 		document.documentElement.scrollTop = document.body.scrollTop = 0;
 	}
 	GoPrint() {
-		if (document.cookie && document.cookie != '') {
-			var cookies = document.cookie.split(';');
-			var name1 = 'user_token'
-			var name2 = 'child_sys_userInfo'
-			var info = {
-				user_token: '',
-				child_sys_userInfo: ''
-			}
-			for (let i = 0; i < cookies.length; i += 1) {
-				var cookie = cookies[i].replace(/(^\s*)|(\s*$)/g, "");
-				if (cookie.substring(0, name1.length + 1) == (name1 + '=')) { 
-					var cookieValue = decodeURIComponent(cookie.substring(name1.length + 1));
-					info.user_token = cookieValue                   
+		TOOLS.get("user/is_logined/").then(res => {
+			if (res.status === 200) {
+				if (res.data.is_logined === true) {
+					window.location.href = "http://www.miningcloud.com.cn/gcfznew/dashboardold/"
+				} else {
+					this.props.history.push("/login")
+					// 页面刷新
+					window.location.reload()
 				}
-				if (cookie.substring(0, name2.length + 1) == (name2 + '=')) { 
-					var cookieValue = decodeURIComponent(cookie.substring(name2.length + 1));
-					info.child_sys_userInfo = cookieValue                    
-				}   
 			}
-
-			if (info.user_token != "" && info.child_sys_userInfo != "") {
-				TOOLS.post("user/is_logined/", info).then(res => {
-					if (res.status === 200) {
-						if (res.data.is_logined === true) {
-							window.location.href = "http://www.miningcloud.com.cn/gcfznew/dashboardold/"
-						} else {
-							// this.props.history.push("/login")
-							// 页面刷新
-							// window.location.reload()
-							document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
-							window.location.href = "http://web.miningcloud.com.cn:8031"
-						}
-					}
-				})
-			} else {
-				document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
-				window.location.href = "http://web.miningcloud.com.cn:8031"
-			}
-		} else {
-				document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
-				window.location.href = "http://web.miningcloud.com.cn:8031"			
-		}
+		})
 	}
+
+	// GoPrint() {
+		// if (document.cookie && document.cookie != '') {
+		// 	var cookies = document.cookie.split(';');
+		// 	var name1 = 'user_token'
+		// 	var name2 = 'child_sys_userInfo'
+		// 	var info = {
+		// 		user_token: '',
+		// 		child_sys_userInfo: ''
+		// 	}
+		// 	for (let i = 0; i < cookies.length; i += 1) {
+		// 		var cookie = cookies[i].replace(/(^\s*)|(\s*$)/g, "");
+		// 		if (cookie.substring(0, name1.length + 1) == (name1 + '=')) { 
+		// 			var cookieValue = decodeURIComponent(cookie.substring(name1.length + 1));
+		// 			info.user_token = cookieValue                   
+		// 		}
+		// 		if (cookie.substring(0, name2.length + 1) == (name2 + '=')) { 
+		// 			var cookieValue = decodeURIComponent(cookie.substring(name2.length + 1));
+		// 			info.child_sys_userInfo = cookieValue                    
+		// 		}   
+		// 	}
+
+		// 	if (info.user_token != "" && info.child_sys_userInfo != "") {
+		// 		TOOLS.post("user/is_logined/", info).then(res => {
+		// 			if (res.status === 200) {
+		// 				if (res.data.is_logined === true) {
+		// 					window.location.href = "http://www.miningcloud.com.cn/gcfznew/dashboardold/"
+		// 				} else {
+		// 					// this.props.history.push("/login")
+		// 					// 页面刷新
+		// 					// window.location.reload()
+		// 					document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
+		// 					window.location.href = "http://web.miningcloud.com.cn:8031"
+		// 				}
+		// 			}
+		// 		})
+		// 	} else {
+		// 		document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
+		// 		window.location.href = "http://web.miningcloud.com.cn:8031"
+		// 	}
+		// } else {
+		// 		document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
+		// 		window.location.href = "http://web.miningcloud.com.cn:8031"			
+		// }
+	// }
 	render() {
 		const { size } = this.state;
 		return (
