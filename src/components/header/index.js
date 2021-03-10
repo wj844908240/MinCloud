26f825487fbc5ae42831c25c773d2692a50b5d63
git <charlielbj@163.com>
2021-03-10 14:46:34 +0800

    wangji

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,6 +10,50 @@ export default class Header extends Component {
         sessionStorage.setItem("current", "");
         window.location.reload()
     }
+    GoPrint() {
+		if (document.cookie && document.cookie != '') {
+			var cookies = document.cookie.split(';');
+			var name1 = 'user_token'
+			var name2 = 'child_sys_userInfo'
+			var info = {
+				user_token: '',
+				child_sys_userInfo: ''
+			}
+			for (let i = 0; i < cookies.length; i += 1) {
+				var cookie = cookies[i].replace(/(^\s*)|(\s*$)/g, "");
+				if (cookie.substring(0, name1.length + 1) == (name1 + '=')) { 
+					var cookieValue = decodeURIComponent(cookie.substring(name1.length + 1));
+					info.user_token = cookieValue                   
+				}
+				if (cookie.substring(0, name2.length + 1) == (name2 + '=')) { 
+					var cookieValue = decodeURIComponent(cookie.substring(name2.length + 1));
+					info.child_sys_userInfo = cookieValue                    
+				}   
+			}
+
+			if (info.user_token != "" && info.child_sys_userInfo != "") {
+				TOOLS.post("user/is_logined/", info).then(res => {
+					if (res.status === 200) {
+						if (res.data.is_logined === true) {
+							window.location.href = "http://www.miningcloud.com.cn/console"
+						} else {
+							// this.props.history.push("/login")
+							// 页面刷新
+							// window.location.reload()
+							document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
+							window.location.href = "http://digital.miningcloud.com.cn:8031?ser_from_mianPage=http://www.miningcloud.com.cn/console"
+						}
+					}
+				})
+			} else {
+				document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
+				window.location.href = "http://digital.miningcloud.com.cn:8031?ser_from_mianPage=http://www.miningcloud.com.cn/console"
+			}
+		} else {
+				document.cookie = "user_from_mianPage=http://www.miningcloud.com.cn/gcfznew/dashboardold/;domain=miningcloud.com.cn"
+				window.location.href = "http://digital.miningcloud.com.cn:8031?ser_from_mianPage=http://www.miningcloud.com.cn/console"			
+		}
+	}
     render(){  
       let { match } = this.props;
         return(
@@ -20,7 +64,8 @@ export default class Header extends Component {
                     </div>
                     <div className="h_right">
                         <ul>
-                            <li><a href="http://www.miningcloud.com.cn/console">管理控制台</a>|</li>
+                            {/* <li><a href="http://www.miningcloud.com.cn/console"  onClick={e => this.GoPrint()}>管理控制台</a>|</li> */}
+                            <li><a href="javascript:void(0)"  onClick={e => this.GoPrint()}>管理控制台</a>|</li>
                             <li><a href="http://techinfo.miningcloud.com.cn">行业资讯</a></li>
                             {/* <li onClick={e=>this.handClick()}><Link to={"/container/new"}>行业资讯</Link>|</li> */}
                             {/* <li onClick={e=>this.handClick()}><Link to={"/container/help"}>帮助中心</Link>|</li>
